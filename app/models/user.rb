1f7e1629c6bdb6459a01b6f3c485afb2278c8295
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-01 19:06:36 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true #nameが空の状態で保存できない
 end
