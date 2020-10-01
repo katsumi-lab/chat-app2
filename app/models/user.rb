6e8ad2a1ad1719cb2c4a2c08e243180f94183987
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-01 20:53:47 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :name, presence: true #nameが空の状態で保存できない
+
+  has_many :room_user
+  has_many :rooms,through: :room_users
 end
