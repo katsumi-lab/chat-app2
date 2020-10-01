71038a0cb345f17bf1e782abbf64ae0d28712824
katsumi-lab <71427533+katsumi-lab@users.noreply.github.com>
2020-10-01 17:57:23 +0900

    ユーザー認証機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+  #deviseのコントローラーが呼び出されたらconfigure_permitted_parametersが呼ばれる。
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters #deviseでユーザー登録をする際に追加したnameカラムのキーの内容を許可
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
 end
